efe1a477885ff496924cc4d4ab4701217b60d9e5
iajatpgf <130942747+iajatpgf@users.noreply.github.com>
2024-04-21 15:55:45 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import streamlit as st
 
 from langchain.memory import ConversationBufferMemory
-from utils import qa_agent
+from ut2 import qa_agent
 
 
-st.title("📑 AI智能PDF问答工具")
+st.title("📑 AI智能文档问答工具")
 
 with st.sidebar:
     openai_api_key = st.text_input("请输入OpenAI API密钥：", type="password")
     st.markdown("[获取OpenAI API key](https://platform.openai.com/account/api-keys)")
+    st.subheader('请选择所要使用的模型')
+    "默认使用gpt-3.5模型，若想得到更好的回复，请选择gpt-4模型"
+    selected_model = st.sidebar.selectbox('选择一个模型', ['gpt-3.5-turbo', 'gpt-4-turbo', 'gpt-4-0125-preview'],
+                                          key='selected_model')
 
 if "memory" not in st.session_state:
     st.session_state["memory"] = ConversationBufferMemory(
@@ -17,15 +21,18 @@ if "memory" not in st.session_state:
         output_key="answer"
     )
 
-uploaded_file = st.file_uploader("上传你的PDF文件：", type="pdf")
-question = st.text_input("对PDF的内容进行提问", disabled=not uploaded_file)
+with st.sidebar:  # 添加一个按钮，点击后重新设置会话状态
+    st.markdown('<p style="color:green; font-size: 24px;">重新开始请按F5</p>', unsafe_allow_html=True)
+
+uploaded_file = st.file_uploader("上传你的文件，支持word，excel，PowerPoint，pdf，txt：", type=["pdf", "txt", "docx", "pptx", "xlsx"])
+question = st.text_input("对文档的内容进行提问,按回车键结束", disabled=not uploaded_file)
 
 if uploaded_file and question and not openai_api_key:
     st.info("请输入你的OpenAI API密钥")
 
 if uploaded_file and question and openai_api_key:
     with st.spinner("AI正在思考中，请稍等..."):
-        response = qa_agent(openai_api_key, st.session_state["memory"],
+        response = qa_agent(selected_model, openai_api_key, st.session_state["memory"],
                             uploaded_file, question)
     st.write("### 答案")
     st.write(response["answer"])
---
+++ ut2.py
@@ -0,0 +1,63 @@
+import os
+import tempfile
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader, UnstructuredPowerPointLoader, \
+    UnstructuredExcelLoader
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def qa_agent(selected_model, openai_api_key, memory, uploaded_file, question):
+    model = ChatOpenAI(model=selected_model, openai_api_key=openai_api_key)
+
+    file_content = uploaded_file.read()
+    file_name = uploaded_file.name
+    file_extension = os.path.splitext(file_name)[1].lower()  # 获取文件扩展名并转为小写
+
+    # 使用临时文件以兼容不同的文件加载器
+    with tempfile.NamedTemporaryFile(delete=False, suffix=file_extension) as temp_file:
+        temp_file.write(file_content)
+        temp_file_path = temp_file.name
+
+    # 根据文件扩展名选择合适的加载器
+    if file_extension == '.pdf':
+        loader = PyPDFLoader(temp_file_path)
+    elif file_extension == '.txt':
+        loader = TextLoader(temp_file_path)
+    elif file_extension == '.docx':
+        loader = Docx2txtLoader(temp_file_path)
+    elif file_extension == '.pptx':
+        loader = UnstructuredPowerPointLoader(temp_file_path)
+    elif file_extension == '.xlsx':
+        loader = UnstructuredExcelLoader(temp_file_path)
+    else:
+        os.unlink(temp_file_path)  # 删除临时文件
+        return "不支持的文件类型。"
+
+    docs = loader.load()
+    os.unlink(temp_file_path)  # 使用完毕后删除临时文件
+
+    # 文本分割
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=50,
+        separators=["\n", "。", "！", "？", "，", "、", ""]
+    )
+    texts = text_splitter.split_documents(docs)
+
+    # 建立文本的向量嵌入并创建快速检索系统
+    embeddings_model = OpenAIEmbeddings()
+    db = FAISS.from_documents(texts, embeddings_model)
+    retriever = db.as_retriever()
+
+    # 创建对话检索链
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=model,
+        retriever=retriever,
+        memory=memory
+    )
+
+    # 执行问答
+    response = qa.invoke({"chat_history": memory, "question": question})
+    return response
